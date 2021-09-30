3f3f296b512eb7a4674a25ec15d2f93a11c8c32b
Cindy Lu <lulu@redhat.com>
2021-09-30 13:49:38 +0800

    clog

---
+++ clag.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# pktgen.conf -- Sample configuration for send on two devices on a UP system
+
+
+git diff -U0 --no-color HEAD~$1^ | //home/lulu/2452/clang/tools/clang-format/clang-format-diff.py  -style file -i -p1
